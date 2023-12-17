c8f6d62708c98c9fe392aac7934878ab7673add6
Aboelkhair93 <ahmed.aboelkhair14@gmail.com>
2023-12-18 00:13:03 +0200

    first terraform exercise

---
+++ first-instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "intro" {
+  ami                    = "ami-0759f51a90924c166"
+  instance_type          = "t2.micro"
+  key_name               = "terr-key"
+  availability_zone      = "us-east-1a"
+  vpc_security_group_ids = ["sg-0e4558dace17eb452"]
+  tags = {
+    Name = "first-terr"
+  }
+}
