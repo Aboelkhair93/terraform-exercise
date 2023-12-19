38d32cd0ca124a8d24163302007b0a583c0d0bf9
Aboelkhair93 <ahmed.aboelkhair14@gmail.com>
2023-12-19 15:45:40 +0200

    second terraform exercise

--- exercise2/instance.tf
+++ exercise2/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "mysec-instance" {
-  ami = var.AMIS[var.REGION]
-  instance_type = "t2.micro"
-  availability_zone = var.ZONE1
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  availability_zone      = var.ZONE1
   key_name               = "terr-key"
   vpc_security_group_ids = ["sg-0e4558dace17eb452"]
   tags = {
