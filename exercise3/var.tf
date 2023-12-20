9a6c0d18d58ace06d3aab8c8d4f1c754db8590c1
Aboelkhair93 <ahmed.aboelkhair14@gmail.com>
2023-12-20 23:05:34 +0200

    Third terraform exercise

---
+++ exercise3/var.tf
@@ -0,0 +1,16 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+variable "AMIS" {
+  type = map(any)
+  default = {
+    #us-east-2 = "ami-0ee4f2271a4df2d7d"
+    us-east-1 = "ami-0759f51a90924c166"
+  }
+}
+variable USER {
+  default = "ec2-user"
+}
